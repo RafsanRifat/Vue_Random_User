b693211621753c16b9b0292afef3f3f1439185c3
RafsanRifat <rafsan.rifat255@gmail.com>
2022-05-22 15:20:44 +0600

    getUser() working

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const app = Vue.createApp({
-    data(){
-        return{
+    data() {
+        return {
             firstName: "Paul",
             lastName: "Thomas",
-            email : "paul@gmail.com",
+            email: "paul@gmail.com",
             picture: "https://randomuser.me/api/portraits/men/10.jpg"
+        }
+    },
+    methods: {
+        getUser() {
+            this.firstName = "Anika",
+            this.lastName = "Tai",
+            this.email = "anika@gmail.com",
+            this.picture = "https://randomuser.me/api/portraits/women/10.jpg"
         }
     }
 })
